41a2b1846f29cf1a1663f23c863378ba7f45b349
Koki-Scripts <fivemucet12@gmail.com>
2025-08-17 19:14:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Koki-Scripts'
+description 'FiveM script, který vypíná falldamage pro hráče.'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
